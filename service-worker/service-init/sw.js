cecd6efb52e2cf55c2d8381a6824800d0c47bcea
sumitkumar91 <sumit.kumar@mobiotics.com>
2019-02-13 10:29:42 +0530

    sw added with activate event

--- service-worker/service-init/sw.js
+++ service-worker/service-init/sw.js
@@ -4,15 +4,26 @@ var cache_list = ['/', './beach-exotic-holiday-248797.jpg']
 
 self.addEventListener("install", (event) => {
   
-     console.log("type", typeof event);
+     console.log("type",  event);
+     // NOTE  In service workers, waitUntil() tells the browser that work is ongoing until the promise settles, and it shouldn't terminate the service worker if it wants that work to complete.
         event.waitUntil(
             caches.open(cache_name).then((cache) => {
                  cache.addAll(cache_list);
             })
         );
+});
 
-        
-      
-}, (error) => {
-
-})
+self.addEventListener('activate', (event) => {
+     console.log("caches", caches);
+     event.waitUntil(
+         caches.keys().then((cacheNames) => {
+             return Promise.all(
+                 cacheNames.filter((name) => {
+                     return name !== cache_name;
+                 }).map((name) => {
+                     //return caches.delete(name);
+                 })
+             )
+         })
+     )
+ });
